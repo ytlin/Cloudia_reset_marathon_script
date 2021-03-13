fea8227cfc58f50b8e99fc2f1b9f35c75aa49340
ytlin1130 <eq180aaa@gmail.com>
2021-03-14 01:11:31 +0800

    first test version using external adb.exe client

---
+++ bot.py
@@ -0,0 +1,70 @@
+from core.helper import helper
+import os
+from time import sleep
+import ntpath
+import time
+
+def check_req(h, req_imgs):
+    accs = []
+    for img in req_imgs:
+        acc, _ = h.match_template(img)
+        accs.append(acc)
+
+    return accs
+
+def check_opt(h, opt_imgs, min):
+    accs = []
+    cnt = 0
+    for img in opt_imgs:
+        acc, _ = h.match_template(img)
+        accs.append(acc)
+        if acc > 0.7:
+            cnt += 1
+
+
+    return (accs, cnt)
+
+OPT_MIN = 1
+
+start_time = time.time()
+
+project_path = os.path.dirname(os.path.abspath(__file__))
+h = helper(project_path)
+temps = h.load_templates()
+reqs = [ntpath.basename(path) for path in h.required_list]
+opts = [ntpath.basename(path) for path in h.optional_list]
+times = 1
+
+while True:
+    # 跳過動畫
+    while True:
+        skip_acc, skip_loc = h.match_template(temps['skip'])
+        re_acc, re_loc = h.match_template(temps['re'])
+        if(skip_acc < 0.7 and re_acc > 0.7):
+            break
+        if(skip_acc > 0.7 and re_acc < 0.7):
+            h.tap_screen(skip_loc[0], skip_loc[1])
+
+    # 開始檢查是否成功
+    #if check_req(h, temps['required']) and check_opt(h, temps['optional'], OPT_MIN):
+    #    break
+    print('=====  {} ========='.format(times))
+    req_accs = check_req(h, temps['required'])
+    opt_accs, cnt = check_opt(h, temps['optional'], OPT_MIN)
+    print('必要:')
+    for i in range(len(reqs)):
+        print('{}: {}'.format(reqs[i], req_accs[i]))
+    print('選中:')
+    for i in range(len(opts)):
+        print('{}: {}'.format(opts[i], opt_accs[i]))
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    print()
+    if all(acc > 0.7 for acc in req_accs) and cnt >= OPT_MIN:
+        break
+    # 再來一次
+    h.tap_screen(re_loc[0], re_loc[1])
+    times += 1
+
+print('Total {} times'.format(times))
+print('Total runtime {} secs'.format(time.time() - start_time))
+
---
+++ core/helper.py
@@ -0,0 +1,80 @@
+import os
+import subprocess
+from cv2 import cv2
+import glob
+import numpy as np
+
+class adb():
+    def __init__(self, path):
+        #[TO_CHECK] first time adb need to do: adb connect localhost:5555 ?
+        self.path = path ##
+        self.adb_exe = '{}\\platform-tools\\adb.exe'.format(self.path)
+
+    def connect_to_BS(self):
+        out = subprocess.Popen('{} connect localhost:5555'.format(self.adb_exe), stdout=subprocess.PIPE)
+        return out.stdout.read()
+
+    def list_devices(self):
+        out = subprocess.Popen('{} devices'.format(self.adb_exe), stdout=subprocess.PIPE)
+        return out.stdout.read()
+
+    def screencap(self):
+        out = subprocess.Popen('{} shell screencap -p'.format(self.adb_exe), stdout=subprocess.PIPE)
+        bytes_arr = np.frombuffer(out.stdout.read().replace(b'\r\n', b'\n'), dtype='uint8')
+        img = cv2.imdecode(bytes_arr, cv2.IMREAD_COLOR)
+       
+        return img
+
+    def tap(self, x, y):
+        out = subprocess.Popen('{} shell input tap {} {}'.format(self.adb_exe, x, y), stdout=subprocess.PIPE)
+        print('===tap: ({}, {})'.format(x, y))
+        return out.stdout.read()
+
+
+def cv_imread(path):
+    img = cv2.imdecode(np.fromfile(path, dtype=np.uint8), cv2.IMREAD_COLOR)
+    return img
+
+class helper():
+
+    def __init__(self, path):
+        self.path = path
+        self.adb = adb(self.path)
+        self.templates = {}
+        self.required_list = glob.glob('{}\\images\\required\\*.png'.format(self.path))
+        self.optional_list = glob.glob('{}\\images\\optional\\*.png'.format(self.path))
+
+    def load_templates(self):
+        # load re.png
+        self.templates['re'] = cv_imread(glob.glob('{}\\images\\re.png'.format(self.path))[0])
+        # load skip.png
+        self.templates['skip'] = cv_imread(glob.glob('{}\\images\\skip.png'.format(self.path))[0])
+        # load required list
+        self.templates['required'] = [cv_imread(f) for f in self.required_list]
+        # load optional list
+        self.templates['optional'] = [cv_imread(f) for f in self.optional_list]
+
+        return self.templates
+
+    def match_template(self, template):
+        '''
+        bg_img = self.adb.screencap()
+        h, w, _ = template.shape
+        res = cv2.matchTemplate(bg_img, template, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv2.rectangle(bg_img,top_left, bottom_right, 255, 2)
+        cv2.imshow('Result', bg_img)
+        cv2.waitKey(0)
+        '''
+        bg_img = self.adb.screencap()
+        res = cv2.matchTemplate(bg_img, template, cv2.TM_CCOEFF_NORMED)
+        _, max_val, _, max_loc = cv2.minMaxLoc(res)
+        return (max_val, max_loc)
+
+    def tap_screen(self, x, y):
+        self.adb.tap(x, y)
+        return 1
+
+
